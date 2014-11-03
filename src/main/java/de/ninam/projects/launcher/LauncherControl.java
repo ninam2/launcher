27b4d27719b0acb961f964738fc398332a293a7c
ninam2 <nina-marie.mueller@web.de>
2014-11-03 16:16:20 +0100

    broken, does not work

---
+++ src/main/java/de/ninam/projects/launcher/LauncherControl.java
@@ -0,0 +1,45 @@
+package de.ninam.projects.launcher;
+
+import javax.usb.UsbException;
+import java.io.IOException;
+
+public class LauncherControl {
+
+    public static void main(String[] args) throws IOException, UsbException {
+
+        final LauncherService launcherService = new LauncherService();
+
+        System.out.println("Welcome to launcher control!");
+        System.out.println("Press...");
+        System.out.println("   w Button to move Launcher up");
+        System.out.println("   s Button to move Launcher down");
+        System.out.println("   a Button to move Launcher left");
+        System.out.println("   d Button to move Launcher right");
+        System.out.println("   space Button to launch");
+        System.out.println("   x Button to exit program");
+
+        char c;
+        while ((c = (char) System.in.read()) != 'x') {
+
+            switch (c) {
+                case 'w':
+                    launcherService.up();
+                    break;
+                case 's':
+                    launcherService.down();
+                    break;
+                case 'a':
+                    launcherService.left();
+                    break;
+                case 'd':
+                    launcherService.right();
+                    break;
+                case ' ':
+                    launcherService.launch();
+                    break;
+            }
+        }
+
+        System.out.println("exiting...");
+    }
+}
