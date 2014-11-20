a974295047e42ecec207c05a28bec8b1b8eb53a6
nmuelle2 <nina-marie.mueller@web.de>
2014-11-20 09:36:28 +0100

    launcher-update

--- src/main/java/de/ninam/projects/launcher/LauncherControl.java
+++ src/main/java/de/ninam/projects/launcher/LauncherControl.java
@@ -2,6 +2,7 @@ package de.ninam.projects.launcher;
 
 import java.io.IOException;
 
+
 public class LauncherControl {
 
     public static void main(String[] args) throws IOException {
@@ -10,6 +11,12 @@ public class LauncherControl {
 
         System.out.println("Welcome to launcher control!");
         System.out.println("Press...");
+        System.out.println("   1 Button to move Launcher to the  top right corner");
+        System.out.println("   2 Button to move Launcher to the  bottom right corner");
+        System.out.println("   3 Button to move Launcher to the  bottom left corner");
+        System.out.println("   4 Button to move Launcher to the  top left corner");
+        System.out.println("   1 Button to move Launcher to the  bottom right corner");
+        System.out.println("   t Button to move Launcher to all corners");
         System.out.println("   w Button to move Launcher up");
         System.out.println("   s Button to move Launcher down");
         System.out.println("   a Button to move Launcher left");
@@ -27,6 +34,38 @@ public class LauncherControl {
         while ((c = (char) System.in.read()) != 'x') {
 
             switch (c) {
+
+                case '0':
+                    launcherService.zero();
+                    break;
+
+                case '1':
+                    launcherService.rightup();
+                    launcherService.launch();
+                    break;
+
+                case '2':
+                    launcherService.rightdown();
+                    launcherService.launch();
+                    break;
+
+
+                case '3':
+                    launcherService.leftdown();
+                    launcherService.launch();
+                    break;
+
+                case '4':
+                    launcherService.leftup();
+                    launcherService.launch();
+                    break;
+
+                case 't':
+                    launcherService.algorithmus();
+
+
+
+
                 case 'w':
                     launcherService.up();
                     break;
