5bfc9e785f238d454f4f3715f93336a59aeda632
nmuelle2 <nina-marie.mueller@web.de>
2014-11-21 14:30:06 +0100

    more tests

--- src/test/java/de/ninam/projects/launcher/console/LauncherControlTest.java
+++ src/test/java/de/ninam/projects/launcher/console/LauncherControlTest.java
@@ -1,6 +1,7 @@
 package de.ninam.projects.launcher.console;
 
 import de.ninam.projects.launcher.LauncherService;
+import de.ninam.projects.launcher.targets.Targets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -35,20 +36,124 @@ public class LauncherControlTest {
     @Test
     public void testShutDown() {
 
-        // TODO
+        // call shutDown
+        launcherControl.shutDown();
+
+        // make sure that ledOff was called on service
+        verify(launcherService, times(1)).ledOff();
     }
 
     @Test
     public void testExecuteCommandZero() {
 
-        // TODO
+        // call ExecuteCommand('0')
+        launcherControl.executeCommand('0');
+
+        // make sure that zero() was called on service
+        verify(launcherService, times(1)).zero();
     }
+
 
     @Test
     public void testExecuteCommandUp() {
 
-        // TODO
+        // call ExecuteCommand('w')
+        launcherControl.executeCommand('w');
+
+        // make sure that up() was called on service
+        verify(launcherService, times(1)).up();
     }
 
-    // TODO
+    @Test
+    public void testExecuteCommandDown() {
+
+        // call ExecuteCommand('s')
+        launcherControl.executeCommand('s');
+
+        // make sure that down() was called on service
+        verify(launcherService, times(1)).down();
+    }
+
+    @Test
+    public void testExecuteCommandLeft() {
+
+        // call ExecuteCommand('a')
+        launcherControl.executeCommand('a');
+
+        // make sure that left() was called on service
+        verify(launcherService, times(1)).left();
+    }
+
+    @Test
+    public void testExecuteCommandRight() {
+
+        // call tExecuteCommand('d')
+        launcherControl.executeCommand('d');
+
+        // make sure that right() was called on service
+        verify(launcherService, times(1)).right();
+    }
+
+    @Test
+    public void testExecuteCommandLaunch() {
+
+        // call tExecuteCommand(' ')
+        launcherControl.executeCommand(' ');
+
+        // make sure that launch was called on service
+        verify(launcherService, times(1)).launch();
+    }
+
+    @Test
+    public void testExecuteCommandTargetOne() {
+
+        // call tExecuteCommand('1')
+        launcherControl.executeCommand('1');
+
+        // make sure that Targets.one was called on service
+        verify(launcherService, times(1)).autoShoot(Targets.one);
+    }
+
+    @Test
+    public void testExecuteCommandTargetTwo() {
+
+        // call ExecuteCommand('2')
+        launcherControl.executeCommand('2');
+
+        // make sure that Targets.two was called on service
+        verify(launcherService, times(1)).autoShoot(Targets.two);
+    }
+
+    @Test
+    public void testExecuteCommandTargetThree() {
+
+        // call  ExecuteCommand('3')
+        launcherControl.executeCommand('3');
+
+        // make sure that Targets.three was called on service
+        verify(launcherService, times(1)).autoShoot(Targets.three);
+    }
+
+    @Test
+    public void testExecuteCommandTargetFour() {
+
+        // call ExecuteCommand('4')
+        launcherControl.executeCommand('4');
+
+        // make sure that Targets.four was called on service
+        verify(launcherService, times(1)).autoShoot(Targets.four);
+    }
+
+    @Test
+    public void testExecuteCommandTargetsAll() {
+
+        // call txecuteCommand('t')
+        launcherControl.executeCommand('t');
+
+        // make sure that all targets were called on service
+        verify(launcherService, times(1)).autoShoot(Targets.one);
+        verify(launcherService, times(1)).autoShoot(Targets.two);
+        verify(launcherService, times(1)).autoShoot(Targets.three);
+        verify(launcherService, times(1)).autoShoot(Targets.four);
+    }
 }
